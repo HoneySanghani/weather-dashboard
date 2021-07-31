43d8cd57ab022a87da6291887f56c23fc6fff50c
Honey Sanghani <honeysanghani98@gmail.com>
2021-07-31 02:55:24 -0400

    icons and five day weather added

--- assests/js/script.js
+++ assests/js/script.js
@@ -11,6 +11,8 @@ var liWindEl=document.querySelector('#list-wind');
 var liHumidityEl=document.querySelector('#list-humidity');
 var liUvEl=document.querySelector('#list-uv');
 
+var heading=document.querySelector("#current-heading");
+
 var divFiveDay=document.querySelector("#five-day-weather");
 
 var fiveEl=document.querySelector("#five-day");
@@ -63,20 +65,23 @@ var getWeather=function(cityName){
             displayWeather(data);
         })
     })
-    var fiveDayApi="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&units=imperial&APPID=a1ebf05a20a8fd712b4baf5c960acf21"
-    fetch(fiveDayApi).then(function(response){
-        response.json().then(function(data){
-            fiveDayDisplay(data);
-        })
-    })
 }
 var displayWeather=function(data){
-    //display single data for the weather
+    //load icon
+    var weatherCode=data.weather[0].icon;
+    var icon="http://openweathermap.org/img/wn/"+weatherCode+".png";
+    var imgEl=document.querySelector("#icon");
+    imgEl.setAttribute("src",icon);
+    heading.appendChild(imgEl);
+
+    //for current weather
     weatherCurrentEl.classList="border p-2";
     headingEl.textContent=data.name+" ("+moment.unix(data.dt).format("MM/DD/YYYY")+")";
-    liTempEl.textContent="Temp: "+data.main.temp+" F";
+    liTempEl.textContent="Temp: "+data.main.temp+"° F";
     liWindEl.textContent="Wind: "+data.wind.speed+" MPH";
     liHumidityEl.textContent="Humidity: "+data.main.humidity+" %";
+
+
 
     //get UV index
     var uvIndexApi="https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&exclude=hourly,minutely&appid=a1ebf05a20a8fd712b4baf5c960acf21";
@@ -85,19 +90,33 @@ var displayWeather=function(data){
            liUvEl.textContent="UV index: "+data.current.uvi;
         })
     })
+    var fiveDayApi="https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&units=imperial&APPID=a1ebf05a20a8fd712b4baf5c960acf21";
+    fetch(fiveDayApi).then(function(response){
+        response.json().then(function(data){
+            fiveDayDisplay(data);
+        })
+    })
 }
 var fiveDayDisplay=function(data){
     //display five day weather
     fiveEl.textContent="5-Day Forecast:";
-    for(var i=4;i<=36;i=i+8){
-        var date=moment.unix(data.list[i].dt).format("MM/DD/YYYY");
+    var x=data.daily;
+    console.log(x.length);
+    for(var i=1;i<6;i++){
+        var date=moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
         var divEl=document.createElement("div");
+        divEl.classList="main-div m-3 p-3 border";
         var h4El=document.createElement("h4");
         h4El.textContent=date;
         divEl.appendChild(h4El);
-        var arr=[data.list[i].main.temp,data.list[i].wind.speed,data.list[i].main.humidity];
+        var imageIcon=data.daily[i].weather[0].icon;
+        var icon="http://openweathermap.org/img/wn/"+imageIcon+".png";
+        var imgEl=document.createElement("img");
+        imgEl.setAttribute("src",icon);
+        divEl.appendChild(imgEl);
+        var arr=[data.daily[i].temp.day,data.daily[i].wind_speed,data.daily[i].humidity];
         var arrName=["Temp: ","Wind: ","Humidity: "];
-        var arrSymbol=["F","MPH","%"];
+        var arrSymbol=["° F"," MPH"," %"];
         for(var j=0;j<arr.length;j++){
             var pEl=document.createElement("p");
             pEl.textContent=arrName[j]+arr[j]+arrSymbol[j];
